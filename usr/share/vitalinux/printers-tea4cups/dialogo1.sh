815217776f978fee346432c9e74b94a353676c5f
amartinr1977 <amartinromero@gmail.com>
2017-06-02 13:27:03 +0200

    ok

--- usr/share/vitalinux/printers-tea4cups/dialogo1.sh
+++ usr/share/vitalinux/printers-tea4cups/dialogo1.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+
+. /etc/default/vx-dga-variables/vx-dga-variables-general.conf
 TEXTO="---"
-yad --title "Gestión de Impresión en Vitalinux" \
+RESPUESTA=$(yad --title "Gestión de Impresión en Vitalinux" \
 	--center \
 	--window-icon "vitalinux" --image "impresora-multifuncion" \
 	--text-align "center" \
     --text "${TEXTO}" \
-    --form --field="Código de Impresión" "${USERCODEPRINT}" \
-    --button="Imprimir:0" \
-    | cut -d"|" -f1 | tee /tmp/codusuario-tea4cups-${TEAUSERNAME}
+    --form \
+    --field="Nombre de Usuario: " "${NOMBREPRINT}" \
+    --field="Código de Impresión" "${USERCODEPRINT}" \
+    --button="Imprimir:0")
+
+if test $? -eq 0 ; then
+	echo "${RESPUESTA}" | cut -d"|" -f1 | tee /tmp/nombreprint-tea4cups-${TEAUSERNAME}
+	echo "${RESPUESTA}" | cut -d"|" -f2 | tee /tmp/codusuario-tea4cups-${TEAUSERNAME}
+else
+	exit 1
+fi
